31806625d2f5aa7848d38a17dc745f57f0655cfd
Nathan099-dev <tonininathan8@gmail.com>
2024-12-05 19:02:57 -0300

    refazendo js

---
+++ aplication/js/script.js
@@ -0,0 +1,12 @@
+function converterMoeda(){
+    const valorInserido = document.querySelector('input');
+    const moedaSelecionada = document.querySelector('select');
+
+    if (moedaSelecionada == 'Dolar') {
+        valorInserido * 6.01
+
+    }else if (moedaSelecionada == 'Euro') {
+        valorInserido * 6.37
+    }
+
+}
