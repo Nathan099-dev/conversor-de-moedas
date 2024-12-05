a91f257b8e95133cddc1a7f17f78d63b60a0a737
Nathan099-dev <tonininathan8@gmail.com>
2024-12-05 14:30:26 -0300

    iniciando código javascript

---
+++ aplication/js/script.js
@@ -0,0 +1,6 @@
+function converterMoeda(){
+    const moedaInicial = document.querySelector('#moeda-entrada');
+    const moedaFinal = document.querySelector('#moeda-saida');
+    const resultadoMoeda = document.querySelector('rusultado-valor-convertido')
+}
+
