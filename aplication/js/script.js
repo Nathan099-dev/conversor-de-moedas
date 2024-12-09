c04dd67cb82e4ede3c30e8bc221cd5397a05da68
Nathan099-dev <tonininathan8@gmail.com>
2024-12-09 18:16:45 -0300

    alteações gerais

--- aplication/js/script.js
+++ aplication/js/script.js
@@ -1,17 +1,41 @@
 function converterMoeda(){
-    const entradaValor = document.querySelector('#valor_entrada_usuario');
-    const selecionarMoeda = document.querySelector('option');
-    const valorConvertido = document.querySelector('#resultado_valor_convertido');
+    const entradaValor = document.getElementById('valor_entrada_usuario');
+    const moedaSelecionada = document.getElementById('moeda_saída_select');
+    const valorConvertido = document.getElementById('resultado_valor_convertido');
+
+    const valorInicial = parseFloat(entradaValor.value);
+    const moedaConvertida = moedaSelecionada.value;
 
     const valorDolar = 6.01;
     const valorEuro = 6.37;
 
-    if (selecionarMoeda == 'Dolar') {
-        let calculoDolar = entradaValor * valorDolar;
-        valorConvertido.innerHTML = `O valor total em dolar é de ${calculoDolar}`;
+    console.log(valorInicial);
+    console.log(moedaSelecionada.value);
+ 
 
-    }else if (selecionarMoeda == 'Euro') {
-        let calculoEuro = entradaValor * valorEuro;
-        valorConvertido.innerHTML = `O valor total em euro é de ${calculoEuro}`;
+    if (isNaN(valorInicial) || valorInicial <= 0) {
+        alert('Por favor, insira um valor válido.');
+        valorConvertido.style.border = '1px solid red';
+        conversaoElement.innerHTML = ''; 
+        return;
+
+    } else {
+        valorConvertido.style.border = ''; 
+    }
+
+    if (moedaConvertida === '2') {
+        const conversaoParaDolar = (valorInicial * valorDolar).toFixed(2);
+        console.log(conversaoParaDolar)
+        valorConvertido.innerHTML = `O valor total em dolar é de $<strong>${conversaoParaDolar}</strong>`;
+
+    }else if (moedaConvertida === '3') {
+        const conversaoParaEuro = (valorInicial * valorEuro).toFixed(2);
+        valorConvertido.innerHTML = `O valor total em euro é de $<strong>${conversaoParaEuro}</strong>`;
+
+    }else{
+        alert('Por favor, selecione uma moeda válida.');
+        valorConvertido.style.border = '1px solid red';
+        conversaoElement.innerHTML = '';
+        return;
     }
 }
